20e92b3c5d3c1ba7a7d7cc716058cfd4472ea544
דביר קליין <dvirklein88@gmail.com>
2024-11-05 21:26:31 +0200

    added storage to amplify

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,15 @@
+import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+
+const backend = defineBackend({
+  auth,
+  data,
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: "<region>",
+    bucket_name: "eu-north-1",
+  },
+});
