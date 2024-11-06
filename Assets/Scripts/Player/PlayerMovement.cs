c8d09df5c580f65b71d87d2e2db2ca14a821996e
Ohad Klein <ohadklein11@gmail.com>
2024-11-06 18:52:22 +0200

    movement perfected

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [SerializeField] private Camera mainCamera;
         [SerializeField] private float movementSpeed = 5f;
         private Animator _animator;
         private Rigidbody2D _rb;
@@ -31,24 +32,35 @@ namespace Player
 
         private void CheckInput()
         {
-            if (Input.GetMouseButtonDown(0))
+            // first try and check for keyboard input
+            _movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            if (_movementDirection.magnitude > 0)
             {
                 _animator.SetBool(AnimationMoving, true);
-            }
-            if (Input.GetMouseButton(0))
-            {
-                // get player position relative to parent
-                Vector2 playerPos = transform.localPosition;
-                // get mouse position, relative to middle of screen
-                Vector2 mousePos = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
-                // get direction normalized vector from player to mouse
-                _movementDirection = (mousePos - playerPos).normalized;
                 SetFacingDirection();
             }
-            if (Input.GetMouseButtonUp(0)) // Left mouse button released
+            else  // if no keyboard input, check for mouse input
             {
-                _animator.SetBool(AnimationMoving, false);
-                _movementDirection = Vector2.zero;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    _animator.SetBool(AnimationMoving, true);
+                }
+                else if (Input.GetMouseButton(0))
+                {
+                    Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+                    // get player position relative to middle of screen
+                    Vector2 playerPos = mainCamera.WorldToScreenPoint(transform.position) - screenCenter;
+                    // get mouse position, relative to middle of screen
+                    Vector2 mousePos = Input.mousePosition - screenCenter;
+                    // get direction normalized vector from player to mouse
+                    _movementDirection = (mousePos - playerPos).normalized;
+                    SetFacingDirection();
+                }
+                else // no input
+                {
+                    _animator.SetBool(AnimationMoving, false);
+                    _movementDirection = Vector2.zero;
+                }
             }
         }
         
@@ -56,20 +68,20 @@ namespace Player
         {
             // set facing direction based on direction angle
             float angle = Vector2.SignedAngle(Vector2.up, _movementDirection);
-            if (angle is > -45 and <= 45)
+            if (angle is > -45 and < 45)
             {
                 _animator.SetInteger(AnimationFacing, (int)PlayerFacingDirection.Up);
             }
-            else if (angle is > 45 and <= 135)
+            else if (angle is >= 45 and <= 135)
             {
                 _animator.SetInteger(AnimationFacing, (int)PlayerFacingDirection.Right);
                 _spriteRenderer.flipX = true;
             }
-            else if (angle is > 135 or <= -135)
+            else if (angle is > 135 or < -135)
             {
                 _animator.SetInteger(AnimationFacing, (int)PlayerFacingDirection.Down);
             }
-            else if (angle is > -135 and <= -45)
+            else if (angle is >= -135 and <= -45)
             {
                 _animator.SetInteger(AnimationFacing, (int)PlayerFacingDirection.Left);
                 _spriteRenderer.flipX = false;
