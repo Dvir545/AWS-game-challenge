3b4be428280896598e96295ef2d693d0d8c819e5
Ohad Klein <ohadklein11@gmail.com>
2024-11-06 14:34:19 +0200

    player direction

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,19 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.Serialization;
 using Utils;
+using Debug = UnityEngine.Debug;
 
 namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float movementSpeed = 5f;
+        private Animator _animator;
         private Rigidbody2D _rb;
         public int facingDirection;
+        private static readonly int Beans = Animator.StringToHash("beans");
 
         private Vector2 _movementDirection;
         private bool _isMoving;
@@ -16,6 +21,7 @@ namespace Player
         void Start()
         {
             _rb = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
             facingDirection = (int)PlayerFacingDirection.Down;
         }
 
@@ -28,14 +34,45 @@ namespace Player
         {
             if (Input.GetMouseButtonDown(0)) // Left mouse button pressed
             {
+                _isMoving = true;
                 // get player position relative to parent
-                Vector3 playerPos = transform.position;
+                Vector2 playerPos = transform.localPosition;
                 // get mouse position, relative to middle of screen
-                Vector3 mousePos = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
+                Vector2 mousePos = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
                 // get direction normalized vector from player to mouse
-                Vector3 direction = (mousePos - playerPos).normalized;
-                Debug.Log(direction);
+                _movementDirection = (mousePos - playerPos).normalized;
+                SetFacingDirection();
+                Debug.Log(_movementDirection);
+            } else
+            {
+                _movementDirection = Vector2.zero;
             }
+        }
+        
+        private void SetFacingDirection()
+        {
+            if (_movementDirection.x > 0)
+            {
+                facingDirection = (int)PlayerFacingDirection.Right;
+            }
+            else if (_movementDirection.x < 0)
+            {
+                facingDirection = (int)PlayerFacingDirection.Left;
+            }
+            else if (_movementDirection.y > 0)
+            {
+                facingDirection = (int)PlayerFacingDirection.Up;
+            }
+            else if (_movementDirection.y < 0)
+            {
+                facingDirection = (int)PlayerFacingDirection.Down;
+            }
+            // animator.SetInteger("FacingDirection", facingDirection);
+        }
+
+        private void Move()
+        {
+            _rb.linearVelocity = _movementDirection * movementSpeed;
         }
     }
 }
