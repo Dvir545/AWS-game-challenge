ca2eb8bd7807f99367db81a6f2669a7dbbbf20ee
ohadklein <ohad.klein@mail.huji.ac.il>
2024-12-11 00:25:35 +0200

    NPC with speech bubble!

---
+++ Assets/Scripts/AWSUtils/NPCSpeech.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace AWSUtils
+{
+    public class NPCSpeech: MonoBehaviour
+    {
+        [SerializeField] private SpeechBubbleBehaviour speechBubbleBehaviour;
+        
+        public void Speak(string text) // DVIR - use this method to make the NPC speak
+        {
+            speechBubbleBehaviour.SetText(text);
+        }
+
+        private void Start()  // DVIR - this is just an example for you to experiment with. Delete this when you get the idea.
+        {
+            Speak("Hello there! Nice to meet you! My name is glorbo and I am the npc!");
+        }
+    }
+}
