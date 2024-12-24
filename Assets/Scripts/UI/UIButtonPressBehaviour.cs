1b11fb3aaa0bd317a48f86e62477b77d1373e65a
ohadklein <ohad.klein@mail.huji.ac.il>
2024-12-24 15:01:14 +0200

    button modifications

---
+++ Assets/Scripts/UI/UIButtonPressBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UIButtonPressBehaviour: MonoBehaviour
+    {
+        private Transform _text;
+
+        private void Awake()
+        {
+            _text = transform.GetChild(0);
+        }
+        
+        public void OnPress()
+        {
+            _text.position = new Vector3(_text.position.x, _text.position.y - transform.localScale.x, _text.position.z);
+        }
+        
+        public void OnRelease()
+        {
+            _text.position = new Vector3(_text.position.x, _text.position.y + transform.localScale.x, _text.position.z);
+        }
+    }
+}
