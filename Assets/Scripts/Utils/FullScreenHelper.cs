baa8d8343131a0e479e4139d50303942f56f4e22
ohadklein <ohad.klein@mail.huji.ac.il>
2024-12-27 12:46:31 +0200

    keyboard for webgl?

---
+++ Assets/Scripts/Utils/FullScreenHelper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public class FullScreenHelper: MonoBehaviour
+    {
+        private bool _isFullScreen;
+
+        private void Awake()
+        {
+            _isFullScreen = Screen.fullScreen;
+        }
+
+        private void Update()
+        {
+            if (_isFullScreen != Screen.fullScreen)
+            {
+                WebGLSupport.WebGLWindow.SwitchFullscreen();
+                _isFullScreen = Screen.fullScreen;
+            }
+        }
+    }
+}
