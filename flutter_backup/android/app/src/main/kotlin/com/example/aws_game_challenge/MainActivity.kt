303665288941a9acec6a11826cecb1396d7d0e00
Dvir545 <163293107+Dvir545@users.noreply.github.com>
2024-12-30 13:47:43 +0200

    Delete flutter_backup directory

--- flutter_backup/android/app/src/main/kotlin/com/example/aws_game_challenge/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.aws_game_challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_backup/lib/remote/load_images.dart
+++
@@ -1,9 +0,0 @@
-// here we load all images in the game
-import 'package:flame/extensions.dart';
-import 'package:flame/game.dart';
-
-const String playerSpriteSheet = 'Player/Player_Idle_Run_Death_Anim.png';
-
-Future<Image> loadPlayerSpriteSheet(FlameGame game) async{
-  return await game.images.load(playerSpriteSheet);
-}
