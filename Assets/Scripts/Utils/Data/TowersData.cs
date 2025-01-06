25214ee3b5e784fb95d498699d46372761326976
ohadklein <ohad.klein@mail.huji.ac.il>
2025-01-06 17:39:16 +0200

    increased tower destruction time

--- Assets/Scripts/Utils/Data/TowersData.cs
+++ Assets/Scripts/Utils/Data/TowersData.cs
@@ -33,11 +33,11 @@ namespace Utils.Data
     public class TowersData: Singleton<TowersData>
     {
         private TowerData[] _towersData = {
-            new(TowerMaterial.Wood, 20, 1, 5, 2, 2.5f, 10, 10),
-            new(TowerMaterial.Stone, 50, 2, 5, 3, 3, 15, 15),
-            new(TowerMaterial.Steel, 100, 3, 5, 4, 1.5f, 20, 20),
-            new(TowerMaterial.Gold, 250, 4, 6, 5, 1f, 25, 20),
-            new(TowerMaterial.Diamond, 600, 5, 7, 6, 1f, 30, 25)
+            new(TowerMaterial.Wood, 20, 1, 5, 2, 2.5f, 10, 13),
+            new(TowerMaterial.Stone, 50, 2, 5, 3, 3, 15, 20),
+            new(TowerMaterial.Steel, 100, 3, 5, 4, 1.5f, 20, 27),
+            new(TowerMaterial.Gold, 250, 4, 6, 5, 1f, 25, 27),
+            new(TowerMaterial.Diamond, 600, 5, 7, 6, 1f, 30, 35)
         };
         
         public TowerData GetTowerData(TowerMaterial tower)
