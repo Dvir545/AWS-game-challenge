5d6a614cc619fa7d9b1cf960b5ff453bf1a1eb0f
ohadklein <ohad.klein@mail.huji.ac.il>
2025-01-06 22:59:21 +0200

    Update TutorialPopUpBehaviour.cs

--- Assets/Scripts/UI/TutorialPopUpBehaviour.cs
+++ Assets/Scripts/UI/TutorialPopUpBehaviour.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialPopUpBehaviour : MonoBehaviour
+namespace UI
 {
-    public void ClosePopUp()
+    public class TutorialPopUpBehaviour : MonoBehaviour
     {
-        transform.GetChild(0).gameObject.SetActive(false);
+        public void ClosePopUp()
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 }
